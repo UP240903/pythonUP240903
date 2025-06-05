8bb2aa69f920d990af80024bf7a3387a42f92b00
UP240903 <up240903@alumnos.upa.edu.mx>
2025-06-05 12:21:53 -0600

    inicio del control de versiones

---
+++ ejercicio_1.py
@@ -0,0 +1,29 @@
+import math
+
+print(3+4) #suma
+print(3-4) #resta
+print(3*4) #multipicacion
+print(3%4) #porcentaje
+print(3/4) #divicion 
+print(3**4) #exponente 
+print(3//4) #residuo de la divicion 
+
+#nivel 1.3
+print("alejandro") #nombre
+print("Alejandro y Rocio") # nombre de mi famila 
+print("Mexico") #pais 
+print("estoy disfrutando los 30 deias de python") 
+
+#nivel 1.4
+print(type(10)) #impresion
+print(type(9.8)) #flotar 
+print(type(4-4j)) #Numero complejo
+print(type(['Asabeneh', 'Python', 'Finland'])) #lista
+print(type("alejandro")) #tu nombre
+print(type("alejandro,rocio")) # nombre de mi familia 
+print(type("Mexico")) #pais 
+
+
+
+#nivel 3.1
+print(math.sqrt ((10-2)**2+(8-3)**2))
